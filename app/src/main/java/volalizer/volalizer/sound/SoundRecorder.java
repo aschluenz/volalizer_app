e4903e1cab0658851921265071a51256b6a0975f
Sebastian Urbanek <sebastian.urbanek@posteo.de>
2016-07-22 17:40:52 +0200

    SoundRecorder refraktoriert und kommentiert.

--- app/src/main/java/volalizer/volalizer/sound/SoundRecorder.java
+++ app/src/main/java/volalizer/volalizer/sound/SoundRecorder.java
@@ -3,7 +3,6 @@ package volalizer.volalizer.sound;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
-import android.media.audiofx.AutomaticGainControl;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -17,19 +16,10 @@ public class SoundRecorder {
     private static final int SAMPLE_RATE = 44100;
     private static final int NUMBER_OF_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
-    private Thread recordingThread = null;
     private boolean mISRecording = true;
     private short[] mBuffer;  //buffer element
-
-    private int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
-    private int BytesPerElement = 2;
-
-    private static String mFileName = null;
     private AudioRecord mRecorder;
     private double dbValue = 0;
-
-    private double sum;
-    private int bufferPos;
 
 
     /**
@@ -44,14 +34,11 @@ public class SoundRecorder {
      * prepares buffer for audiorecoder
      */
     private void initRecorder() {
-        int bufferSizePersonal = BufferElements2Rec * BytesPerElement;
         int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
                                                       AudioFormat.ENCODING_PCM_16BIT);
         mBuffer = new short[bufferSize];
         mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE, NUMBER_OF_CHANNELS,
-//                                    RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
                                     RECORDER_AUDIO_ENCODING, bufferSize);
-        Log.e(log, "initRecorder :: bufferSize = " + bufferSize);
     }
 
     public double doRecord() throws InterruptedException {
@@ -91,12 +78,14 @@ public class SoundRecorder {
                     double db = 0.0;
                     int readSize = mRecorder.read(mBuffer, 0, mBuffer.length);
 
+                    // Berechnung der Amplitude
                     for (int i = 0; i < readSize; i++) {
                         double val = Math.abs(mBuffer[i]);
                         if (amplitude < val) {
                             amplitude = val;
                         }
                     }
+                    // Berechnung des Schalldruckpegels
                     db = 20 * Math.log10(amplitude);
                     dbValues.add(db);
                 }
