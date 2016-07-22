ebc54210bec1f2bd6b6654343043f920843ad3ff
Andy Schlünz <andyschluenz@gmail.com>
2016-07-22 10:42:13 +0200

    listadapter onclicklistener

--- app/src/main/java/volalizer/volalizer/managers/ListAdapter.java
+++ app/src/main/java/volalizer/volalizer/managers/ListAdapter.java
@@ -1,7 +1,5 @@
 package volalizer.volalizer.managers;
 
-import android.app.Application;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -12,13 +10,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import volalizer.volalizer.MainActivity;
 import volalizer.volalizer.R;
 import volalizer.volalizer.RecordDetailActivity;
 import volalizer.volalizer.models.Record;
-import volalizer.volalizer.utils.GeocoderHelper;
-
-import static android.support.v4.app.ActivityCompat.startActivity;
 
 /**
  * Created by andyschlunz on 19.06.16.
@@ -27,7 +21,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     private static final String TAG = "ListAdapter";
 
-    private Record[] mDataSet;
+    public static Record[] mDataSet;
 
     @Override
     public void onAttachedToRecyclerView(RecyclerView recyclerView) {
@@ -37,8 +31,6 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.record_list_item, parent,false);
-
-
         return new ViewHolder(view);
 
     }
@@ -58,14 +50,13 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
 
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder{
         TextView location;
         TextView dbValue;
         ImageView Image;
         CardView cv;
 
-
-        public ViewHolder(View itemView) {
+        public ViewHolder(final View itemView) {
             super(itemView);
 
             cv = (CardView) itemView.findViewById(R.id.cv);
@@ -81,15 +72,19 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
                 public void onClick(View v) {
                     Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
 
+                    Intent i = new Intent(v.getContext(), RecordDetailActivity.class);
+                   // i.putExtra("record_details",(android.os.Parcelable) getmDataSet(getAdapterPosition()));
+                    v.getContext().startActivity(i);
                 }
             });
-
-
         }
-
     }
 
     public ListAdapter(Record[] dataSet) {
         mDataSet = dataSet;
+    }
+
+    public static Record getmDataSetPosition(int position){
+        return ListAdapter.mDataSet[position];
     }
 }
